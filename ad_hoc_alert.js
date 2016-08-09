4d3e2fea393974e472badb03ac894664e502ebc8
Sean Fitzpatrick <sffitzpatrick@gmail.com>
2016-08-09 18:08:04 -0500

    change names of new files to match naming convention

--- ad_hoc_alert.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  Drupal.behaviors.gisdAlerts = {
-    attach: function (context, settings) {
-      $.get('ajax/ad-hoc-alert/load/view', function( data ) {
-        $(data).prependTo('body');
-      });
-    }
-  };
-}(jQuery));
